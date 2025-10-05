37a8f693c0368195906a1770ff098949a712dd28
Einar <einargudnig@gmail.com>
2025-10-05 00:45:51 +0000

    improving typing of velite config

--- velite.config.ts
+++ velite.config.ts
@@ -8,20 +8,20 @@ export default defineConfig({
     posts: {
       name: "Post", // collection type name
       pattern: "posts/**/*.{md,mdx}", // content files glob pattern (support both md and mdx)
-      schema: s.object({
-        title: s.string().max(99), // Zod primitive type
-        slug: s.slug("posts"), // validate format, unique in posts collection
-        image: s.image().optional(),
-        // date: s.isodate(), // input Date-like string, output ISO Date string.
-        // cover: s.image().optional(), // input image relative path, output image object with blurImage.
-        // video: s.file().optional(), // input file relative path, output file public path.
-        // metadata: s.metadata(), // extract markdown reading-time, word-count, etc.
-        // excerpt: s.excerpt(), // excerpt of markdown content
-        // content: s.markdown(), // transform markdown to html
-        code: s.mdx(), // compile mdx to js code
-      }),
-      // Add permalink computed field
-      transform: (data) => ({ ...data, permalink: `/blog/${data.slug}` }),
+      schema: s
+        .object({
+          title: s.string().max(99), // Zod primitive type
+          slug: s.slug("posts"), // validate format, unique in posts collection
+          image: s.image().optional(),
+          // date: s.isodate(), // input Date-like string, output ISO Date string.
+          // cover: s.image().optional(), // input image relative path, output image object with blurImage.
+          // video: s.file().optional(), // input file relative path, output file public path.
+          // metadata: s.metadata(), // extract markdown reading-time, word-count, etc.
+          // excerpt: s.excerpt(), // excerpt of markdown content
+          // content: s.markdown(), // transform markdown to html
+          code: s.mdx(), // compile mdx to js code
+        })
+        .transform((data) => ({ ...data, permalink: `/blog/${data.slug}` })),
     },
   },
 });
