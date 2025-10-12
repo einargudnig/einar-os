db70cf2133e0e215745f13db0f4ea1c762febcfd
Einar <einargudnig@gmail.com>
2025-10-12 01:12:55 +0000

    adding to infra

--- app/uses/infrastructure/page.tsx
+++ app/uses/infrastructure/page.tsx
@@ -9,6 +9,16 @@ export default function InfraPage() {
         <li>Arc</li>
         <li>Notion Calendar</li>
         <li>Cursor</li>
+        <li>Bruno</li>
+        <li>Insomnia</li>
+        <li>1Password</li>
+        <li>CleanshotX</li>
+        <li>Rectangle pro</li>
+        <li>homerow.app</li>
+        <li>Vimium</li>
+        <li>karabiner</li>
+        <li>cold turkey</li>
+        <li>Toggl</li>
       </ul>
     </div>
   );
