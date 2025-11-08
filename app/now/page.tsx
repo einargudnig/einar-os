fbace13df863919e7fb497b12d1aa726c0044632
Einar <einargudnig@gmail.com>
2025-11-08 12:02:38 +0000

    minor updates

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -1,58 +1,44 @@
 import { ExternalLink } from "@/components/blog/external-link";
+import { Separator } from "@radix-ui/react-separator";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 export default function Page() {
   return (
     <section className="mx-auto w-full max-w-2xl space-y-8 print:space-y-6 mb-8">
+      <h1 className="font-semibold text-2xl font-serif">Now</h1>
       <div className="flex flex-col">
-        <div className="flex items-center mb-5">
-          <h1 className="font-semibold text-2xl font-serif">Now</h1>
-        </div>
-
-        <p className="-mt-4 text-neutral-500">Updated 27. jan, 2025</p>
+        <p className="-mt-4 text-neutral-500">Updated 08. Nov, 2025</p>
       </div>
       <div className="prose prose-neutral dark:prose-invert text-neutral-200">
-        <p className="text-lg">Writing</p>
-        <p className="-mt-2">
-          I recently moved my writing from this page to me new Astro page. I
-          have wanted to write more for the last couple of years and I have
-          never managed to make it important enough. I have been planning some
-          posts for me to write in the future so check out&nbsp;
-          <ExternalLink href="https://writing.einargudni.com">
-            my writings
-          </ExternalLink>
-          .
+        <p className="text-neutral-500 text-lg font-mono">Writing</p>
+        <p>
+          I made some updates to my homepage. Simplified it a lot. I had set up
+          a specific project on Astro to handle my writings. It was a fun
+          experiment but I didn&apos;t like having separate projects. Now I have
+          moved everything back and I will continue to add things here.
         </p>
-        <p className="text-lg">Craft</p>
-        <p className="-mt-2">
-          I have seen other people post their own UI component creations and it
-          inspires my a lot. My bookmarks on Twitter are full of delightful UI
-          components that I want to try to recreate and add to my{" "}
-          <ExternalLink href="https://craft.einargudni.com">site.</ExternalLink>{" "}
-          This seems like a fun way to spend an evening here and there.
-        </p>
-        <p className="text-lg">Fundamentals</p>
-        <p className="-mt-2">
+      </div>
+      <Separator />
+      <div className="prose prose-neutral dark:prose-invert text-neutral-200">
+        <p className="text-neutral-500 text-lg font-mono">Fundamentals</p>
+        <p>
           I feel like I have some holes in the fundamentals when I am writing
           code, it slows my annoyingly down. I will spend time on this to
           improve. This is super important to me. I feel like I&apos;ve made
-          some progress here, I have some notes I want to share!
+          some progress here, recently I have been diving into TypeScript.
         </p>
-        <p className="text-lg">HYROX</p>
-        <p className="-mt-2">
-          Hyrox was great! We didn&apos;t manage to achieve our goal of going
-          under 60 minutes, we were 4 minutes away. Now we are back to training,
-          mainly focusing on functional fitness, strength and aerobic base I
-          think we will do another HYROX later to try to beat our time.
-        </p>
-        <p className="text-lg">Reading</p>
-        <p className="-mt-2">
-          I am listening to the fourth book in the{" "}
-          <ExternalLink href="https://www.goodreads.com/book/show/15839976-red-rising">
-            Red Rising
+      </div>
+      <Separator />
+      <div className="prose prose-neutral dark:prose-invert text-neutral-200">
+        <p className="text-neutral-500 text-lg font-mono">Reading</p>
+        <p>
+          I am listening to{" "}
+          <ExternalLink href="https://www.goodreads.com/book/show/17994.The_Code_Book">
+            The Code Book: Science of Secrecy from Ancient Egypt to Quantum
+            Cryptography
           </ExternalLink>{" "}
-          saga, by Pierce Brown, and I am obsessed! I love it!
+          I just started but it seems super interesting so far!
         </p>
       </div>
       <div className="flex justify-start">
