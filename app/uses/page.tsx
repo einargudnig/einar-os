c939a3fb598353fa389a18c53ea312172a3f1adc
Einar <einargudnig@gmail.com>
2025-12-25 11:47:55 +0000

    preload image

--- app/uses/page.tsx
+++ app/uses/page.tsx
@@ -4,7 +4,7 @@ import desk from "../../public/images/desk.png";
 export default function Page() {
   return (
     <div>
-      <Image src={desk} alt="Desk" className="rounded-md" />
+      <Image src={desk} alt="Desk" className="rounded-md" preload />
       <p className="mt-3 text-xs text-neutral-500">
         Point and shoot photo of the current setup
       </p>
