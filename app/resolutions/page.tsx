c0017c8d3506aee47b22de05d8e7531cdaa0940b
Einar <einargudnig@gmail.com>
2025-12-29 22:08:42 +0000

    adding resolutions

---
+++ app/resolutions/page.tsx
@@ -0,0 +1,28 @@
+import { Checkbox } from "@/components/ui/checkbox";
+
+export default function ResolutionsPage() {
+  return (
+    <section className="mx-auto w-full max-w-2xl space-y-8 print:space-y-6 mb-8">
+      <h1 className="font-semibold text-2xl font-serif">Resolutions</h1>
+
+      <div className="prose prose-neutral dark:prose-invert text-neutral-200">
+        <p className="text-neutral-500 text-lg font-mono">2026</p>
+        <ul className="mt-4">
+          <li className="flex items-center gap-2">
+            <Checkbox disabled />
+            <p className="">Become a dad!</p>
+          </li>
+          <li className="flex items-center gap-2">
+            <Checkbox disabled />
+            <p className="">sub 75 minute pro single HYROX </p>
+          </li>
+
+          <li className="flex items-center gap-2">
+            <Checkbox disabled />
+            <p className="">Create a native app</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
